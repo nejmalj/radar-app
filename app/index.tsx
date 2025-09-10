5f8fbee8a06ef4869118e8308a2f5cae59b2e461
nejmalj <nejmalejeannic@gmail.com>
2025-09-10 10:14:41 +0200

    User position with permission

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,64 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function Index() {
+    const [location, setLocation] = useState(null);
+    const [region, setRegion] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                return 'Permission refusée';
+            }
+
+            let currentLocation = await Location.getCurrentPositionAsync({});
+            setLocation(currentLocation.coords);
+
+            setRegion({
+                latitude: currentLocation.coords.latitude,
+                longitude: currentLocation.coords.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            });
+
+            Location.watchPositionAsync(
+                {
+                    accuracy: Location.Accuracy.High,
+                    timeInterval: 2000,
+                    distanceInterval: 5,
+                },
+                (pos) => {
+                    setLocation(pos.coords);
+                    setRegion({
+                        latitude: pos.coords.latitude,
+                        longitude: pos.coords.longitude,
+                        latitudeDelta: 0.01,
+                        longitudeDelta: 0.01,
+                    });
+                }
+            );
+        })();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
+            {region && (
+                <MapView style={styles.map} region={region} showsUserLocation={true}>
+                    {location && (
+                        <Marker
+                            coordinate={{
+                                latitude: location.latitude,
+                                longitude: location.longitude,
+                            }}
+                            title="Vous êtes ici"
+                            pinColor="blue"
+                        />
+                    )}
+                </MapView>
+            )}
         </View>
     );
 }
@@ -15,7 +68,7 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     map: {
-        width: '100%',
-        height: '100%',
+        width: "100%",
+        height: "100%",
     },
 });
